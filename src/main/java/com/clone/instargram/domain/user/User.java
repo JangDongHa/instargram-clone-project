bad103be8590b02cc24fc65ff35b2bfa12f4e47e
mury33@naver.com <mury33@naver.com>
2022-08-20 14:00:38 +0900

    branchtest01

--- src/main/java/com/clone/instargram/domain/user/User.java
+++ src/main/java/com/clone/instargram/domain/user/User.java
@@ -14,6 +14,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 public class User extends Timestamped {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
